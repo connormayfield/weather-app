91cd163f94b4d5fd63484c13c3995d438edfe09a
connormayfield <connormayfield@gmail.com>
2019-06-26 01:19:25 -0500

    fetch works

--- app.js
+++ app.js
@@ -7,16 +7,15 @@ window.addEventListener("load", () => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = "https://cors-anywhere.herokuapp.com";
+      const proxy = "https://cors-anywhere.herokuapp.com/";
       const api = `${proxy}https://api.darksky.net/forecast/04bf3ace72997ad39fd8b5be2a5edd8f/${lat},${long}`;
 
       fetch(api)
-        .then(respons =>) {
-            return Response.json();
+        .then(response => {
+          return response.json();
         })
-        .then(data =>{
-            console.log(data);
-            dfasdf
+        .then(data => {
+          console.log(data);
         });
     });
   }
