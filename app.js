b57549bb36c109597106b43d6f7dffb6d6d573ac
connormayfield <connormayfield@gmail.com>
2019-06-26 01:57:42 -0500

    done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ window.addEventListener("load", () => {
   );
   let temperatureDegree = document.querySelector(".temperature-degree");
   let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -23,12 +25,24 @@ window.addEventListener("load", () => {
           console.log(data);
           const { temperature, summary, icon } = data.currently;
           //   Set DOM Elements from the API
-          temperatureDegree.textContent = temperature;
+          temperatureDegree.textContent = Math.floor(temperature);
           temperatureDescription.textContent = summary;
           locationTimezone.textContent = data.timezone;
-
+          // FORMULA FOR CELSIUS
+          let celsius = (temperature - 32) * (5 / 9);
           //SetIcon
           setIcons(icon, document.querySelector(".icon"));
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
+
+          //Change temperature to Celsius/Farenheit
         });
     });
   }
